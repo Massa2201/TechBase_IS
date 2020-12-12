ef89ecf84f330df284e11663254452c100b5a5e4
Imai Masashi <chrono.trigger.2424@gmail.com>
2020-12-13 03:21:22 +0900

    flockの学習（書き込みに応じて番号を更新する）

---
+++ mission3/mission_3-1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>mission_3-1</title>
+</head>
+<body>
+
+<form action = "" method = "post">
+    <input type = "text" name = "text">
+    <input type = "submit" name = "button01">
+</form>
+<?php
+    $text = $_POST["text"];
+    $filename="mission_3-1.txt";
+    date_default_timezone_set('Asia/Tokyo');
+    $date = date("Y/m/d H:i分:");
+    $user_name = "phpくん"; 
+    
+    if (isset ($_POST["text"]) ) {
+        if(!empty($text)) {
+        $fp = fopen($filename,"r+");
+        flock($fp, LOCK_EX);
+        while (fgets($fp) !== false) {
+            $count++;
+        }
+        $next = $count + 1;
+        $board = $next ."<>" .$user_name . "<>" . $text . "<>" . $date;
+        fwrite($fp,$board . PHP_EOL);
+        flock($fp, LOCK_UN);
+        fclose($fp);
+        echo $filename . "が更新されました";
+        }
+        else {
+            echo "コメントが空です";
+        }
+    }else {
+        echo "コメントを送信してください！";
+    }
+?>
+
+</body>
+</html>
