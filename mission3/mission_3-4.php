75e467203689f9df271a4932aa1c8ec03972a7eb
Imai Masashi <chrono.trigger.2424@gmail.com>
2020-12-20 02:56:22 +0900

    3－3コピー

---
+++ mission3/mission_3-4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>mission_3-4</title>
+    </head>
+    <body>
+
+    <p>新規投稿フォーム</p>
+    <form action = "" method = "post">
+        <input type = "text" name = "text">
+        <input type = "submit" name = "button01">
+    </form>
+
+    <p>投稿削除フォーム</p>
+    <form action = "" method = "post">
+        <input type = "text" name = "num">
+        <input type = "submit" name = "button02">
+    </form>
+    
+<?php
+    
+?>
+
+    </body>
+</html>
